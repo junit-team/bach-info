c147fe6de3fcd64af529bf05404bc610019cd3c3
Christian Stein <sormuras@gmail.com>
2024-05-23 14:07:17 +0000

    Include EPL v2.0 header

--- package-info.java
+++ package-info.java
@@ -1,2 +1,12 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
 /** Information about modules and tools provided by JUnit 5.11.0-M2 */
 package run.info.org.junit;
