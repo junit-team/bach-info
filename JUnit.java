7ec9b8eef1dc0b950a85e79fd4c984744c768148
Christian Stein <sormuras@gmail.com>
2024-05-28 16:02:17 +0200

    Report supported module names

--- JUnit.java
+++ JUnit.java
@@ -22,8 +22,26 @@ public interface JUnit {
   static ModuleLocator modules() {
     return ModuleLocator.compose(
         ModuleLocator.of("org.apiguardian.api", "pkg:maven/org.apiguardian/apiguardian-api@1.1.2"),
-        new JUnitLocator("org.junit.jupiter", "5.11.0-M2"),
-        new JUnitLocator("org.junit.platform", "1.11.0-M2"),
+        JUnitLocator.of(
+            "org.junit.jupiter",
+            "5.11.0-M2",
+            "org.junit.jupiter",
+            "org.junit.jupiter.api",
+            "org.junit.jupiter.engine",
+            "org.junit.jupiter.params"),
+        JUnitLocator.of(
+            "org.junit.platform",
+            "1.11.0-M2",
+            "org.junit.platform.commons",
+            "org.junit.platform.console",
+            "org.junit.platform.engine",
+            "org.junit.platform.jfr",
+            "org.junit.platform.launcher",
+            "org.junit.platform.reporting",
+            "org.junit.platform.suite",
+            "org.junit.platform.suite.api",
+            "org.junit.platform.suite.commons",
+            "org.junit.platform.suite.engine"),
         ModuleLocator.of("org.opentest4j", "pkg:maven/org.opentest4j/opentest4j@1.3.0"));
   }
 }
--- JUnitLocator.java
+++ JUnitLocator.java
@@ -1,14 +1,17 @@
 package run.info.org.junit;
 
+import java.util.Set;
 import run.bach.ModuleLocator;
 
-record JUnitLocator(String group, String version) implements ModuleLocator {
+record JUnitLocator(Set<String> names, String group, String version) implements ModuleLocator {
+  static JUnitLocator of(String group, String version, String... names) {
+    return new JUnitLocator(Set.of(names), group, version);
+  }
+
   @Override
-  public Location locate(String module) {
-    if (module.startsWith(group)) {
-      var artifact = module.substring("org.".length()).replace('.', '-');
-      return Location.of(module, "pkg:maven/" + group + "/" + artifact + "@" + version);
-    }
-    return Location.unknown(module);
+  public Location locate(String name) {
+    if (!names().contains(name)) return Location.unknown(name);
+    var artifact = name.substring("org.".length()).replace('.', '-');
+    return Location.of(name, "pkg:maven/" + group + "/" + artifact + "@" + version);
   }
 }
